aabd33192343982ba58185661865c4f240288e77
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-12-18 10:27:57 -0500

    commit tree 5dc211cb22f8399b0b1128585b9c482da0d1b58c

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,24 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    @student = Student.create(student_params)
+    redirect_to "/students/#{params[:student][:id]}"
+  end
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
   end
 
 end
